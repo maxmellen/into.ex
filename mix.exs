035d30b7e1c113691916e801afb4f6e87427dcbe
Maximilien Mellen <maxmellen0@gmail.com>
2017-12-10 16:52:22 +0100

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -7,7 +7,10 @@ defmodule Into.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
+      source_url: "https://github.com/maxmellen/into.ex"
     ]
   end
 
@@ -23,6 +26,18 @@ defmodule Into.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "Collect the result of a pipe expression into a variable with ~>"
+  end
+
+  defp package() do
+    [
+      maintainers: ["Maximilien Mellen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/maxmellen/into.ex"}
     ]
   end
 end
